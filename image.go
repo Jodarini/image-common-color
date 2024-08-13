9c753cacac4151160ae171a58d8ace7f535dee00
jodarini <rivasjose9428@gmail.com>
2024-08-12 19:37:51 -0500

    Refactor: Create getCommonColor func

--- image.go
+++ image.go
@@ -4,24 +4,18 @@ import (
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	_ "image/png"
 	"os"
 )
 
-type rgba struct {
+type rgb struct {
 	red   uint32
 	green uint32
 	blue  uint32
 }
 
-func get_most_used_color() {
-}
-
-func main() {
-	reader, err := os.Open("./image/Designer.jpeg")
-	if err != nil {
-		panic(err)
-	}
-	m, _, err := image.Decode(reader)
+func getMostUsedColor(r *os.File) rgb {
+	m, _, err := image.Decode(r)
 	if err != nil {
 		panic(err)
 	}
@@ -36,12 +30,25 @@ func main() {
 
 	for x := bounds.Min.X; x < bounds.Max.X; x += 100 {
 		r, g, b, _ := m.At(x, 0).RGBA()
+		count += 1
 		r_total += r >> 8
 		g_total += g >> 8
 		b_total += b >> 8
-		count += 1
-		fmt.Println(count)
-
-		fmt.Printf("red: %v \n green: %v \n blue: %v \n", r_total/count, g_total/count, b_total/count)
 	}
+
+	r_total = r_total / count
+	g_total = g_total / count
+	b_total = b_total / count
+
+	return rgb{r_total, g_total, b_total}
+}
+
+func main() {
+	reader, err := os.Open("./image/CuteCat.png")
+	if err != nil {
+		panic(err)
+	}
+	rgb := getMostUsedColor(reader)
+
+	fmt.Printf("red: %v \ngreen: %v \nblue: %v \n", rgb.red, rgb.green, rgb.blue)
 }
